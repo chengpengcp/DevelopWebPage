8fd6e6ab39fb36f3617b934749ded5fda3bc4ec4
程鹏 <chengpengcp@outlook.com>
2018-11-18 21:31:04 +0800

    创建初始 Django 应用

--- DevelopDjangoWebPage/DevelopDjangoWebPage/urls.py
+++ DevelopDjangoWebPage/DevelopDjangoWebPage/urls.py
@@ -3,12 +3,16 @@ Definition of urls for DevelopDjangoWebPage.
 """
 
 from django.conf.urls import include, url
+# cp add code 2018118
+import HelloworldDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$',HelloworldDjangoApp.views.index,name="index"),
+    url(r'^home$',HelloworldDjangoApp.views.index,name="home"),
     # Examples:
     # url(r'^$', DevelopDjangoWebPage.views.home, name='home'),
     # url(r'^DevelopDjangoWebPage/', include('DevelopDjangoWebPage.DevelopDjangoWebPage.urls')),
