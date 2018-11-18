903f5b87de8ac1c325f348a4dd82f07d88602279
程鹏 <chengpengcp@outlook.com>
2018-11-18 21:44:20 +0800

    创建初始 Django 应用

--- DevelopDjangoWebPage/DevelopDjangoWebPage/urls.py
+++ DevelopDjangoWebPage/DevelopDjangoWebPage/urls.py
@@ -3,23 +3,10 @@ Definition of urls for DevelopDjangoWebPage.
 """
 
 from django.conf.urls import include, url
-# cp add code 2018118
 import HelloworldDjangoApp.views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
+# Django processes URL patterns in the order they appear in the array
 urlpatterns = [
-    url(r'^$',HelloworldDjangoApp.views.index,name="index"),
-    url(r'^home$',HelloworldDjangoApp.views.index,name="home"),
-    # Examples:
-    # url(r'^$', DevelopDjangoWebPage.views.home, name='home'),
-    # url(r'^DevelopDjangoWebPage/', include('DevelopDjangoWebPage.DevelopDjangoWebPage.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HelloworldDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloworldDjangoApp.views.index, name='home'),
 ]
--- DevelopDjangoWebPage/HelloworldDjangoApp/views.py
+++ DevelopDjangoWebPage/HelloworldDjangoApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) :
-    return HttpResponse("Helloworld Django Web")
+    return HttpResponse("程鹏 LOVE 卢萍萍")
 
 
 # Create your views here.
